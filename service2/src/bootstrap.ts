6aef3d01c1fafd8dcc856c1ba493050ef6dc3d05
viniazvd <viniazvd@gmail.com>
2022-06-22 10:55:13 -0300

    :hammer: refactor

--- service2/src/bootstrap.ts
+++ service2/src/bootstrap.ts
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
 import 'vue-sdz/dist/css/bundle.css'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app-service')
+new Vue({ render: h => h(App) }).$mount('#app-service')
